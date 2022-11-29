4ad88c69e37a90e9bab8bbca64eb9d253f5b511a
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2022-11-29 11:53:29 +0700

    Initial Commit

---
+++ SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootCampApp.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Nunu Nugraha on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
